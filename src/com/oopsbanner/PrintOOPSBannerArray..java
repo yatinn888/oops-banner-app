b3094caafb0de54193de8f0f48004aca88c11030
yatinn888 <youremail@example.com>
2026-02-24 10:24:24 +0530

    Feature(UC4):PrintOOPSBannerArray

---
+++ src/com/oopsbanner/PrintOOPSBannerArray..java
@@ -0,0 +1,19 @@
+public class PrintOOPSBannerArray {
+    public static void main(String[] args) {
+
+        String[] lines = new String[7];
+
+        lines[0] = String.join(" ", "   ***   ", "   ***   ", " ******  ",  "  *****  ");
+        lines[1] = String.join(" ", " **   ** ", " **   ** ", " **    ** ", " **    **");
+        lines[2] = String.join(" ", "**     **", "**     **", " **     **", "**       ");
+        lines[3] = String.join(" ", "**     **", "**     **", " **    ** ", "  ****   ");
+        lines[4] = String.join(" ", "**     **", "**     **", " ******   ", "      ** ");
+        lines[5] = String.join(" ", " **   ** ", " **   ** ", " **       ", "**     **");
+        lines[6] = String.join(" ", "   ***   ", "   ***   ", " **       ", "  *****  ");
+
+        // Print using for-each loop
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
