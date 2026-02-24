baf1d3e8fbd6a15ffcb9320ae4f0e6ab96c14514
yatinn888 <youremail@example.com>
2026-02-24 10:19:12 +0530

    Feature(UC3):print oops join

---
+++ src/com/oopsbanner/PrintOOPSBannerjoin.java
@@ -0,0 +1,13 @@
+public class PrintOOPSBannerjoin {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", " ******  ", "  *****  "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", " **    ** ", " **    ** "));
+        System.out.println(String.join(" ", "**     **", "**     **", " **     **", "**       "));
+        System.out.println(String.join(" ", "**     **", "**     **", " **    ** ", "  ****   "));
+        System.out.println(String.join(" ", "**     **", "**     **", " ******   ", "      ** "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", " **       ", "**     **"));
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", " **       ", "  *****  "));
+        
+    }
+}
