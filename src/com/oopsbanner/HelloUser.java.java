50a101bc791f30c0a2cbb39e88f7817a8b669a6c
yatinn888 <youremail@example.com>
2026-03-11 13:24:12 +0530

    Feature(UC2): elloUser

---
+++ src/com/oopsbanner/HelloUser.java.java
@@ -0,0 +1,14 @@
+public class HelloUser {
+
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello " + name);
+        } 
+        else {
+            System.out.println("Hello User");
+        }
+
+    }
+}
